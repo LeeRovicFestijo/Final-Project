b739b8af360f366f57962fb3d89ea490998e2c95
LRFestijo <117901938+LeeRovicFestijo@users.noreply.github.com>
2023-11-28 16:24:19 +0800

    Add files via upload

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/LogoDA.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">DepEd: Alternative Learning System - Batangas City</p>
+							<h3>
+
+							<p> A Modular Learning Management System</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>Alternative Learning System:</p>
+												<p>Empowering Out-of-School Filipinos</p>
+												<p>Through Academic Literacy</p>
+								</div>		
+						</div>		
+				</div>
